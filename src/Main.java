b49f325df68c7612c7458e6ff2412a0bcafee413
DmitriiVolkovIzh <booyakaa@mail.ru>
2022-10-26 16:20:08 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+
+public class Main {
+    public static void main(String[] args) {
+        Author authorOne = new Author("Джордж","Оруэлль");
+        Book bookOne = new Book("1984", authorOne, 1949);
+
+        Author authorTwo = new Author("Николай","Носов");
+        Book bookTwo = new Book("Незнайка на луне", authorOne, 1965);
+
+        System.out.println("Книга 1 - " + bookOne.getName() + ", " + authorOne.getName()+ " " + authorOne.getSurname() + ", " + bookOne.setYearOfRelease() );
+        System.out.println("Книга 2 - " + bookTwo.getName() + ", " + authorTwo.getName()+ " " + authorTwo.getSurname() + ", " + bookTwo.setYearOfRelease() );
+    }
+}
